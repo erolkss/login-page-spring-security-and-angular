dbcdb5d0de2925526db9c7b79ce2f79b2dec573d
Lucas Lima <lucaserolima@gmail.com>
2024-03-30 20:11:35 -0400

    feat: Created DTOs for request and response

---
+++ login-auth-api/src/main/java/br/com/ero/loginauthapi/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package br.com.ero.loginauthapi.dto;
+
+public record LoginRequestDTO (String email, String password){
+}
---
+++ login-auth-api/src/main/java/br/com/ero/loginauthapi/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package br.com.ero.loginauthapi.dto;
+
+public record ResponseDTO(String name, String token) {
+}
